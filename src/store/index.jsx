13c284f0a16357332e74306d81ae7f38b7ce0f23
Fi44er <vaceslavmalugin81@gmail.com>
2023-10-26 20:31:38 +0500

    add

--- src/store/index.jsx
+++ src/store/index.jsx
@@ -1,8 +1,8 @@
-import { createStore, combineReducers, applyMiddleware } from "redux"
+import { createStore, combineReducers, applyMiddleware } from "redux" // yarn add redux && yarn add react-redux
 import { cashReducer } from "./cashReduser"
 import { customerReducer } from "./customerReducer"
-import { composeWithDevTools } from "redux-devtools-extension"
-import thunk from 'redux-thunk';
+import { composeWithDevTools } from "redux-devtools-extension" // yarn add redux-devtools-extension
+import thunk from 'redux-thunk'; // yarn add redux-thunk
 
 const rootReducer = combineReducers({
   //объединяем 2 reducer
