8cc93ad3601a15fb968fdf6990d42775b26ecabc
Fi44er <vaceslavmalugin81@gmail.com>
2023-10-27 14:41:38 +0500

    add

--- src/pages/Home.1.jsx
+++ src/pages/Home.1.jsx
@@ -1,14 +1,11 @@
 import useFetch from "react-fetch-hook"
 
-const Home = () => {
+export const Home = () => {
   const { data } = useFetch("https://randomuser.me/api/")
   return (
     <>
       <img src={data?.results[0]?.picture?.medium} />
       <pre>{JSON.stringify(data, null, 2)}</pre>
-      {/* <div>{data?.results[0]?.gender}</div> */}
     </>
   )
 }
-
-export default Home
